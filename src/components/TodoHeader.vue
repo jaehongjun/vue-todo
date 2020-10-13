38112257c9cacae93c3fb8385ab3ba2d1ba9b87b
jaehongjun <gatsby0391@naver.com>
2020-10-14 02:31:21 +0900

    vue init

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,17 @@
+<template>
+<header><h1>TODO it!</h1></header>
+</template>
+
+<script>
+export default {
+// js script
+}
+</script>
+
+<style scoped>
+h1 { 
+    color: darkslateblue;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+}
+</style>
